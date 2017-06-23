67db34afd6e449ba06b7c6418357a1d85a751bd1
Thiparphan J <tppjvk@gmail.com>
2017-06-23 19:48:41 +0700

    element side - ant

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,168 @@
 import React, { Component } from 'react';
 import './Csshome.css'
-
+import { Glyphicon, Button } from 'react-bootstrap';
 class Home extends Component {
   render() {
     return (
-      <div>
+      <div className="flex-container">
         <div id="cut">
-          test Font Branch 2222
+          Font Branch 2222
+        </div>
+        <div className="flex-wrap">
+          <div className="flex-item">
+            <div className="flex-img">
+              img
+            </div>
+            <div>
+              product content
+            </div>
+            <div className="align-butt">
+              <button className="but-cart" value="Purchase">
+                <Glyphicon glyph="shopping-cart" />
+                Add to Cart
+              </button>
+            </div>
+          </div>
+          <div className="flex-item">
+            <div className="flex-img">
+              img
+            </div>
+            <div>
+              product content
+            </div>
+            <div className="align-butt">
+              <button className="but-cart" value="Purchase">
+                <Glyphicon glyph="shopping-cart" />
+                Add to Cart
+              </button>
+            </div>
+          </div>
+          <div className="flex-item">
+            <div className="flex-img">
+              img
+            </div>
+            <div>
+              product content
+            </div>
+            <div className="align-butt">
+              <button className="but-cart" value="Purchase">
+                <Glyphicon glyph="shopping-cart" />
+                Add to Cart
+              </button>
+            </div>
+          </div>
+          <div className="flex-item">
+            <div className="flex-img">
+              img
+            </div>
+            <div>
+              product content
+            </div>
+            <div className="align-butt">
+              <button className="but-cart" value="Purchase">
+                <Glyphicon glyph="shopping-cart" />
+                Add to Cart
+              </button>
+            </div>
+          </div>
+          <div className="flex-item">
+            <div className="flex-img">
+              img
+            </div>
+            <div>
+              product content
+            </div>
+            <div className="align-butt">
+              <button className="but-cart" value="Purchase">
+                <Glyphicon glyph="shopping-cart" />
+                Add to Cart
+              </button>
+            </div>
+          </div>
+          <div className="flex-item">
+            <div className="flex-img">
+              img
+            </div>
+            <div>
+              product content
+            </div>
+            <div className="align-butt">
+              <button className="but-cart" value="Purchase">
+                <Glyphicon glyph="shopping-cart" />
+                Add to Cart
+              </button>
+            </div>
+          </div>
+          <div className="flex-item">
+            <div className="flex-img">
+              img
+            </div>
+            <div>
+              product content
+            </div>
+            <div className="align-butt">
+              <button className="but-cart" value="Purchase">
+                <Glyphicon glyph="shopping-cart" />
+                Add to Cart
+              </button>
+            </div>
+          </div>
+          <div className="flex-item">
+            <div className="flex-img">
+              img
+            </div>
+            <div>
+              product content
+            </div>
+            <div className="align-butt">
+              <button className="but-cart" value="Purchase">
+                <Glyphicon glyph="shopping-cart" />
+                Add to Cart
+              </button>
+            </div>
+          </div>
+          <div className="flex-item">
+            <div className="flex-img">
+              img
+            </div>
+            <div>
+              product content
+            </div>
+            <div className="align-butt">
+              <button className="but-cart" value="Purchase">
+                <Glyphicon glyph="shopping-cart" />
+                Add to Cart
+              </button>
+            </div>
+          </div>
+          <div className="flex-item">
+            <div className="flex-img">
+              img
+            </div>
+            <div>
+              product content
+            </div>
+            <div className="align-butt">
+              <button className="but-cart" value="Purchase">
+                <Glyphicon glyph="shopping-cart" />
+                Add to Cart
+              </button>
+            </div>
+          </div>
+          <div className="flex-item">
+            <div className="flex-img">
+              img
+            </div>
+            <div>
+              product content
+            </div>
+            <div className="align-butt">
+              <button className="btn but-cart" value="Purchase">
+                <Glyphicon glyph="shopping-cart" />
+                Add to Cart
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     );
