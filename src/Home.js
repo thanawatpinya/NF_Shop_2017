6618113c8f27499016d8324a35d6feb9a36fb8da
DESKTOP-RD7FCUM\ThanawatPinya <thanawat_pinya@outlook.com>
2017-06-27 15:43:51 +0700

    Sum code

--- src/Home.js
+++ src/Home.js
@@ -1,43 +1,70 @@
 import React, {Component} from 'react';
 import './Csshome.css'
 import img1 from './img/1.jpeg'
-import { Glyphicon, Button } from 'react-bootstrap';
+import {Glyphicon, Button} from 'react-bootstrap';
+import {DeleteAmount} from './DeleteAmount';
 let countid = 0;
 
 class Home extends Component {
-  constructor(){
+  constructor() {
     super()
-    this.state={
-      itemlist:[
-        {id:0,name:"Iphone1",price:10000,count:1},
-        {id:1,name:"Iphone2",price:20000,count:1},
-        {id:2,name:"Iphone3",price:30000,count:1},
-        {id:3,name:"Iphone4",price:40000,count:1}
+    this.state = {
+      itemlist: [
+        {
+          id: 0,
+          name: "Iphone1",
+          price: 10000,
+          count: 1
+        }, {
+          id: 1,
+          name: "Iphone2",
+          price: 20000,
+          count: 1
+        }, {
+          id: 2,
+          name: "Iphone3",
+          price: 30000,
+          count: 1
+        }, {
+          id: 3,
+          name: "Iphone4",
+          price: 40000,
+          count: 1
+        }
       ],
-      itemcart:[],
+      itemcart: [],
       counter: 0,
       name: "",
-      changecount:0
+      changecount: 0,
+      toggleDelete: false,
+      tempName: "",
+      tempCount: 0,
+      tempID: 0,
+      totalAmount: 0
     }
   }
 
-  bundleAddToCart(e){
-    if(this.state.itemcart.find(a => a.name == this.state.itemlist[e.target.value].name)){
-      for(let i = 0; i < this.state.itemcart.length; i++){
-        if(this.state.itemcart[i].name == this.state.itemlist[e.target.value].name){
+  bundleAddToCart(e) {
+    this.setState({
+      totalAmount: this.state.totalAmount + this.state.itemlist[e.target.value].price
+    })
+    if (this.state.itemcart.find(a => a.name == this.state.itemlist[e.target.value].name)) {
+      for (let i = 0; i < this.state.itemcart.length; i++) {
+        if (this.state.itemcart[i].name == this.state.itemlist[e.target.value].name) {
           var index = i
           break;
         }
       }
       this.setState({
         itemcart: [
-          ...this.state.itemcart, ...this.state.itemcart[index].count = this.state.itemcart[index].count+1, ...this.state.itemcart[index].price = this.state.itemlist[e.target.value].price * this.state.itemcart[index].count
+          ...this.state.itemcart,
+          ...this.state.itemcart[index].count = this.state.itemcart[index].count + 1,
+          ...this.state.itemcart[index].price = this.state.itemlist[e.target.value].price * this.state.itemcart[index].count
         ],
         counter: this.state.counter += this.state.itemlist[e.target.value].price
       })
       index = 0
-      this.state.changecount
-    }else {
+    } else {
       console.log(this.state.itemcart);
       this.setState({
         itemcart: [
@@ -52,21 +79,66 @@ class Home extends Component {
       })
     }
   }
-  handleChangecount(e) {
-    if(this.state.itemcart.find(a => a.name == this.state.itemlist[e.target.id].name)){
-      for(let i = 0; i < this.state.itemcart.length; i++){
-        if(this.state.itemcart[i].name == this.state.itemlist[e.target.id].name){
-          var index = i
-          break;
+  checkItem(path, set) {
+    switch (set) {
+      case "cart":
+        for (let i = 0; i < this.state.itemcart.length; i++) {
+          if (this.state.itemcart[i].name == this.state.itemlist[path].name) {
+            return i
+          }
         }
-      }
-    console.log(this.state.itemcart);
-    itemcart: [...this.state.itemcart, ...this.state.itemcart[e.target.id].count = parseInt(e.target.value), ...this.state.itemcart[e.target.id].price = this.state.itemlist[index].price * this.state.itemcart[e.target.id].count]
-     this.setState({
-
-     })
+      case "count":
+        for (let i = 0; i < this.state.itemlist.length; i++) {
+          if (this.state.itemcart[path].name == this.state.itemlist[i].name) {
+            return i
+          }
+        }
+      default:
+        return 0
+    }
   }
-}
+  handleChangecount(e) {
+    let path = e.target.id
+    let index = this.checkItem(path, "count")
+    if (this.state.itemcart.find(a => a.name == this.state.itemlist[index].name)) {
+      let basePrice = this.state.itemlist[index].price
+      let input = parseInt(e.target.value)
+      itemcart : [
+        ...this.state.itemcart,
+        ...this.state.itemcart[path].count = input,
+        ...this.state.itemcart[path].price = basePrice * this.state.itemcart[path].count
+      ]
+      counter : this.state.counter = this.handleTotalPrice()
+      this.setState({})
+    }
+  }
+  handleTotalPrice() {
+    let sum = 0
+    for (let i = 0; i < this.state.itemcart.length; i++) {
+      sum += this.state.itemcart[i].price
+    }
+    return sum
+  }
+  handleUpdateCart(id) {
+    let item2 = this.state.itemcart.filter(item => item.id === id)
+    this.setState({
+      itemcart: this.state.itemcart.filter(item => item.id !== id),
+      toggleDelete: false,
+      counter: this.state.counter -= item2[0].price
+    })
+    //  totalAmount: this.state.totalAmount - item2[0].price})
+  }
+
+  handleDelete(bool) {
+    this.setState({toggleDelete: bool})
+  }
+  handleSendData(name, count, id) {
+    this.setState({tempName: name, tempCount: count, tempID: id})
+  }
+  handleShowModal(bool, name, count, id) {
+    this.handleDelete(bool)
+    this.handleSendData(name, count, id)
+  }
 
   render() {
     return (
@@ -83,18 +155,28 @@ class Home extends Component {
                 <label>Amount</label>
                 <label>Price</label>
               </div>
-              {this.state.itemcart.map((record, index) =>
-                  <div id="in-cartlist">
-                    <label id="name-item">{record.name}</label>
-                    <input className="setinput" id={index} type="number" value={record.count} onChange={this.handleChangecount.bind(this)}/>
-                    <label>{record.price}</label>
-                  </div>
-              )}
+              {this.state.itemcart.map((record, index) => <div id="in-cartlist">
+                <label id="name-item">{record.name}</label>
+                <input className="setinput" id={index} type="number" value={record.count} onChange={this.handleChangecount.bind(this)}/>
+                <label>{record.price}</label>
+                <div>
+                  <button onClick={this.handleShowModal.bind(this, true, record.name, record.count, record.id)} className="but-delete">
+                    <Glyphicon glyph="trash"/>
+                  </button>
+                </div>
+              </div>)}
             </div>
           </div>
           <div className="checkoutset">
+            {this.state.toggleDelete
+              ? <DeleteAmount handleDelete={this.handleDelete.bind(this)} handleUpdateCart={this.handleUpdateCart.bind(this)} amount={this.state.tempCount} name={this.state.tempName} id={this.state.tempID}/>
+              : null}
             <div id="total-content">
-              <label>Total Price : </label><label> {this.state.counter} </label>
+              <label>Total Price :
+              </label>
+              <label>
+                {this.state.counter}
+              </label>
             </div>
             <div id="button">
               <button className="button button-hover">Checkout</button>
@@ -102,8 +184,7 @@ class Home extends Component {
           </div>
         </div>
         <div className="flex-wrap">
-          {this.state.itemlist.map((record, index) =>
-          <div className="flex-item">
+          {this.state.itemlist.map((record, index) => <div className="flex-item">
             <div className="flex-img">
               <img src={img1} height="200px"></img>
             </div>
@@ -117,12 +198,11 @@ class Home extends Component {
             </div>
             <div className="align-butt">
               <button className="but-cart" value={record.id} onClick={this.bundleAddToCart.bind(this)}>
-                <Glyphicon glyph="shopping-cart" />
+                <Glyphicon glyph="shopping-cart"/>
                 Add to Cart
               </button>
             </div>
-          </div>
-          )}
+          </div>)}
         </div>
       </div>
     );
